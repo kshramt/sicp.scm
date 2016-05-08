0b1871a0d4429bf5c77d85cb15ebf929e8dd46c3
kshramt <thisisdummy@example.com>
2016-05-08 18:45:02 +0900

    Q 4.1

--- my-scheme-lib.scm
+++ my-scheme-lib.scm
@@ -5,5 +5,81 @@
             )
   )
 
-(define (my-eval) #t)
-(define (my-apply) #t)
+
+(define (my-eval exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+        ((assignment? exp) (eval-assignment exp env))
+        ((definition? exp) (eval-definition exp env))
+        ((if? exp) (eval-if exp env))
+        ((lambda? exp) (make-procedure (lambda-parameters exp)
+                                       (lambda-body exp)
+                                       env))
+        ((begin? exp) (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (my-eval (cond->if exp) env))
+        ((application? exp) (my-apply (my-eval (operator exp) env)
+                                      (list-of-values (operands exp) env)))
+        (else (error "Unknown expression type -- EVAL" exp))))
+
+
+(define (my-apply procedure arguments)
+  (cond ((primitive-procedure? procedure)
+         (apply-primitive-procedure procedure arguments))
+        ((compound-procedure? procedure)
+         (eval-sequence
+          (procedure-body procedure)
+          (extend-environment
+           (procedure-parameters procedure)
+           arguments
+           (procedure-environment procedure))))
+        (else
+         (error
+          "Unknown procedure type -- APPLY" procedure))))
+
+
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (my-eval (first-operand exps) env)
+            (list-of-values (rest-operands exps) env))))
+"Q 4.1 ->"
+(define (list-of-values-q-4-1-> exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((a (my-eval (first-operand exps) env)))
+        (cons a
+              (list-of-values (rest-operands exps) env)))))
+"Q 4.1 <-"
+(define (list-of-values-q-4-1<- exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((d (list-of-values (rest-operands exps) env)))
+        (cons (my-eval (first-operand exps) env)
+              d))))
+
+
+(define (eval-if exp env)
+  (if (true? (my-eval (if-predicate exp) env))
+      (my-eval (if-consequent exp) env)
+      (my-eval (if-alternative exp) env)))
+
+
+(define (eval-sequence exps env)
+  (cond ((last-exp? exps) (my-eval (first-exp exps) env))
+        (else (my-eval (first-exp exps) env)
+              (eval-sequence (rest-exps exps) env))))
+
+
+(define (eval-assignment exp env)
+  (set-variable-value! (assignment-variable exp)
+                       (my-eval (assignment-value exp) env)
+                       env)
+  'ok)
+
+
+(define (eval-definition exp env)
+  (define-variable! (definition-variable exp)
+                    (my-eval (definition-value exp) env)
+                    env)
+  'ok)
