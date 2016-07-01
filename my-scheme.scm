736472c2d6f90af09899eac254d4eedfc320bd5d
kshramt <thisisdummy@example.com>
2016-07-02 02:09:15 +0900

    Write

--- my-scheme.scm
+++ my-scheme.scm
@@ -73,6 +73,25 @@
           "Unknown procedure type -- APPLY" procedure))))
 
 
+(define (apply-primitive-procedure proc args)
+  'todo)
+
+(define (primitive-procedure? proc)
+  'todo)
+
+(define (compound-procedure? proc)
+  (tagged-list? proc 'procedure))
+
+
+(define (make-procedure parameters body env)
+  (list 'procedure parameters body env))
+
+
+(define procedure-parameters cadr)
+(define procedure-body caddr)
+(define procedure-environment cadddr)
+
+
 (define true #t)
 
 (define false #f)
@@ -554,6 +573,79 @@
   )
 
 (install-eval-4-3)
+
+
+(define (lookup-variable-value var env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (env-loop (enclosing-environment env)))
+            ((eq? var (car vars))
+             (car vals))
+            (else
+             (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable -- LOOKUP=VARIABLE=VALUE" var)
+        (let ((frame (first-frame env)))
+          (scan (frame-variables frame)
+                (frame-values frame)))))
+  (env-loop env))
+
+
+(define (extend-environment vars vals base-env)
+  (if (= (length vars) (length vals))
+      (cons (make-frame vars vals) base-env)
+      (if (< (length vars) (length vals))
+          (error "Too many arguments supplied" vars vals)
+          (error "Too few arguments supplied" vars vals))))
+
+
+(define (define-variable! var val env)
+  (let ((frame (first-frame env)))
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (add-binding-to-frame! var val frame))
+            ((eq? var (car vars))
+             (set-car! vals val))
+            (else
+             (scan (cdr vars)
+                   (cdr vals)))))
+    (scan (frame-variables frame)
+          (frame-values frame))))
+
+
+(define (set-variable-value! var val env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (env-loop (enclosing-environment env)))
+            ((eq? var (car vars))
+             (set-car! vals val))
+            (else
+             (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable -- SET!" var)
+        (let ((frame (first-frame env)))
+          (scan (frame-variables frame)
+                (frame-values frame)))))
+  (env-loop env))
+
+
+(define (make-frame vars vals)
+  (cons vars vals))
+
+
+(define (frame-variables frame)
+  (car frame))
+
+
+(define (frame-values frame)
+  (cdr frame))
+
+
+(define (add-binding-to-frame! var val frame)
+  (set-car! frame (cons var (frame-variables frame)))
+  (set-cdr! frame (cons val (frame-values frame))))
 
 
 (define (test)
